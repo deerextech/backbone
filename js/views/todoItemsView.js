2de8ac5fb23a0e0071b3bb9b50171221fab25b13
Danielle Stiring <deerextech@gmail.com>
2017-04-26 13:47:42 -0400

    Can now add value via textbox.  Need to add in a keypress event because clicking add button every time is irritating
    	modified:   js/views/todoItemsView.js

--- js/views/todoItemsView.js
+++ js/views/todoItemsView.js
@@ -24,18 +24,31 @@ onAddTodoItem: function(todoItem){
 
 },
 onClickAdd: function(e){
+  var $textBox = this.$("#newTodoItem");
+  //check for empty values
+  if($textBox.val()){
+    //if value, add it
+    var todoItem = new TodoItem({description: this.$($textBox).val()});
+  }
+  // The following 3 lines are for adding a static item... won't need that soon, but keep reference here, just in case
   //when button is clicked, add static item
-  //will need listener to update actual list on webpage
-  var todoItem = new TodoItem({description:"new item"});
-  this.model.add(todoItem);
+  // //will need listener to update actual list on webpage
+  // var todoItem = new TodoItem({description:"new item"});
+
+
+
   //will instruct view to add item in initalize..
+  this.model.add(todoItem);
+
+  //clear textbox
+  this.$($textBox).val("");
 },
 render:function(){
   //need to make a reference for 'this' because its context is going to change inside the item map. #javascriptNinja
   var self = this;
 
   //
-  // this.$el.append("<input type='text' autofocus id='newTodoItem'></input")
+  this.$el.append("<input type='text' autofocus id='newTodoItem'></input")
 
   this.$el.append("<button id='add'>Add</button>");
 
