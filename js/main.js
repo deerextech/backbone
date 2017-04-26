a170a3fa3b70a1878e72cd185529134cc8b3f24b
Danielle Stiring <deerextech@gmail.com>
2017-04-26 12:25:07 -0400

    Added view to render a test item.
    	modified:   index.html
    	modified:   js/main.js
    	modified:   js/views/todoItemView.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 
-  var todoItems = new TodoItems();
+  // var todoItems = new TodoItems();
+  var todoItem = new TodoItem({description: "todo 1"});
+  var todoItemView = new TodoItemView({model: todoItem})
+  $("body").append(todoItemView.render().$el);
 
 });
---
+++ js/views/todoItemView.js
@@ -0,0 +1,19 @@
+//view is focused on changes of todoItem model
+// is interested in changes in model & refreshes itself automatically
+//handles events raised by DOM
+// This view will not know anything about adding items to list
+
+var TodoItemView = Backbone.View.extend({
+
+  initialize:function(options){
+    if(!(options && options.model))
+    //if model isn't passed to a view, this error will be thrown
+    throw new Error("model is not specified");
+
+
+  },
+  render:function(){
+    this.$el.html(this.model.get("description"));
+    return this;
+  }
+})
