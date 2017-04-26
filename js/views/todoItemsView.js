2a79bcb0b68be240713b269461f804c03d0cc20a
Danielle Stiring <deerextech@gmail.com>
2017-04-26 12:49:01 -0400

    Almost forgot to specify a ul tag for items view.. ha. So now it renders list of models (in main.js), into an actual list.
    	modified:   index.html
    	modified:   js/views/todoItemsView.js

--- js/views/todoItemsView.js
+++ js/views/todoItemsView.js
@@ -1,6 +1,9 @@
 //view for collection
 
 var TodoItemsView = Backbone.View.extend({
+
+tagName:"ul",
+
 render:function(){
   //need to make a reference for 'this' because its context is going to change inside the item map. #javascriptNinja
   var self = this;
@@ -9,7 +12,7 @@ render:function(){
     var view = new TodoItemView({model: todoItem});
     //see, the self refers to model
     console.log('this keyword', this);
-    console.log('self variable', self); 
+    console.log('self variable', self);
     self.$el.append(view.render().$el);
   });
 
